a0c336d1eb18edcabcbb02c934432fa0a7b5a76b
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-03-29 23:26:57 +0200

    Added more simple unit tests

--- LexerTests/SimpleMathTests/CalculationTests.cs
+++ LexerTests/SimpleMathTests/CalculationTests.cs
@@ -20,7 +20,7 @@ namespace LexerTests.SimpleMathTests
         }
 
         [Test]
-        public void SimpleCalculation()
+        public void SimpleAddition()
         {
             //Arrange
             string testData = "1+1";
@@ -35,6 +35,60 @@ namespace LexerTests.SimpleMathTests
 
             //Assert
             Assert.That(value, Is.EqualTo(2.0));
+        }
+
+        [Test]
+        public void SimpleSubtraction()
+        {
+            //Arrange
+            string testData = "1-2";
+            SimpleLexer lexer = new SimpleLexer();
+            SimpleParser parser = new SimpleParser();
+            SimpleInterpreter interpreter = new SimpleInterpreter();
+
+            //Act
+            var tokens = lexer.GenerateTokens(testData).ToList();
+            var tree = parser.Parse(tokens);
+            var value = interpreter.Visit(tree);
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1.0));
+        }
+
+        [Test]
+        public void SimpleMultiplication()
+        {
+            //Arrange
+            string testData = "2*16";
+            SimpleLexer lexer = new SimpleLexer();
+            SimpleParser parser = new SimpleParser();
+            SimpleInterpreter interpreter = new SimpleInterpreter();
+
+            //Act
+            var tokens = lexer.GenerateTokens(testData).ToList();
+            var tree = parser.Parse(tokens);
+            var value = interpreter.Visit(tree);
+
+            //Assert
+            Assert.That(value, Is.EqualTo(32.0));
+        }
+
+        [Test]
+        public void SimpleDivision()
+        {
+            //Arrange
+            string testData = "16/2";
+            SimpleLexer lexer = new SimpleLexer();
+            SimpleParser parser = new SimpleParser();
+            SimpleInterpreter interpreter = new SimpleInterpreter();
+
+            //Act
+            var tokens = lexer.GenerateTokens(testData).ToList();
+            var tree = parser.Parse(tokens);
+            var value = interpreter.Visit(tree);
+
+            //Assert
+            Assert.That(value, Is.EqualTo(8.0));
         }
     }
 }
